5b0478b4ea6b167c718dc3a91ddc5e5336cfab1c
YuriyShandor <yuriyshandor@gmail.com>
2017-06-17 20:36:39 +0300

    Changes for script 01

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $(".icon").click(function(){
     $(".icon").removeClass("icon_bold");
     $(this).addClass("icon_bold");
-    var index = $(this).index();
+    var index = $(this).index()+1;
     $(".content").removeClass("show");
     $(".content:nth-child("+ index +")").addClass("show");
     });
