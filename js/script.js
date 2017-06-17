4da4fb12b9c21553b3aca24c5280652933777cd3
YuriyShandor <yuriyshandor@gmail.com>
2017-06-17 19:41:26 +0300

    Changes for script

--- js/script.js
+++ js/script.js
@@ -1,31 +1,9 @@
 $(document).ready(function(){
-  $(".content_1, .content_2, .content_3, .content_4").hide();
-  $(".icon_1").click(function(){
+  $(".icon").click(function(){
+    $(".icon").removeClass("icon_bold");
     $(this).addClass("icon_bold");
-    $(".icon_2, .icon_3, .icon_4").removeClass("icon_bold");
-    $(".content_1").show();
-    $(".content_2, .content_3, .content_4").hide();
-  });
-
-  $(".icon_2").click(function(){
-    $(this).addClass("icon_bold");
-    $(".icon_1, .icon_3, .icon_4").removeClass("icon_bold");
-    $(".content_2").show();
-    $(".content_1, .content_3, .content_4").hide();
-  });
-
-  $(".icon_3").click(function(){
-    $(this).addClass("icon_bold");
-    $(".icon_1, .icon_2, .icon_4").removeClass("icon_bold");
-    $(".content_3").show();
-    $(".content_1, .content_2, .content_4").hide();
-  });
-
-  $(".icon_4").click(function(){
-    $(this).addClass("icon_bold");
-    $(".icon_1, .icon_2, .icon_3").removeClass("icon_bold");
-    $(".content_4").show();
-    $(".content_1, .content_2, .content_3").hide();
-  });
-
+    var index = $(this).index();
+    $(".content").removeClass("show");
+    $(".content:nth-child("+ index +")").addClass("show");
+    });
   });
